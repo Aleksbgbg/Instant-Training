5c93a41581c1987ecc0813d81bc0707f1bb76f4b
Aleksbgbg <kozichka01@abv.bg>
2019-04-11 15:25:56 +0100

    [Constants] Add arena names.

--- Instant Training UI/Constants.cs
+++ Instant Training UI/Constants.cs
@@ -3,5 +3,39 @@
     public static class Constants
     {
         public const string AppName = "Rocket League Instant Training Mod";
+
+        public static readonly string[] ArenaNames =
+        {
+            "ARC",
+            "ARC_Standard",
+            "Beach",
+            "Farm",
+            "HoopsStadium",
+            "Labs_CirclePillars",
+            "Labs_Cosmic",
+            "Labs_DoubleGoal",
+            "Labs_Octagon",
+            "Labs_Underpass",
+            "Labs_Utopia",
+            "NeoTokyo",
+            "NeoTokyo_Standard",
+            "Park",
+            "Park_Night",
+            "Park_Rainy",
+            "ShatterShot",
+            "Stadium",
+            "Stadium_Day",
+            "Stadium_Foggy",
+            "Stadium_Winter",
+            "ThrowbackStadium",
+            "TrainStation",
+            "TrainStation_Dawn",
+            "TrainStation_Night",
+            "Underwater",
+            "UtopiaStadium",
+            "UtopiaStadium_Dusk",
+            "UtopiaStadium_Snow",
+            "Wasteland"
+        };
     }
 }
