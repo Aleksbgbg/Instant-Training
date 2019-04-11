66e841d56a1ad68ec82a3da9d5e433a01b79fe81
Aleksbgbg <kozichka01@abv.bg>
2019-04-11 16:15:26 +0100

    [ArenaDisplayViewModel] Add arena display image, with the ability to cycle between arenas.

---
+++ Instant Training UI.Tests/ViewModels/ArenaDisplayViewModelTests.cs
@@ -0,0 +1,85 @@
+﻿namespace Instant.Training.UI.Tests.ViewModels
+{
+    using Instant.Training.UI.ViewModels;
+
+    using Xunit;
+
+    public class ArenaDisplayViewModelTests
+    {
+        private readonly ArenaDisplayViewModel _arenaDisplayViewModel;
+
+        public ArenaDisplayViewModelTests()
+        {
+            _arenaDisplayViewModel = new ArenaDisplayViewModel();
+        }
+
+        [Fact]
+        public void TestDefaultArenaIsIndexZero()
+        {
+            Assert.Equal(Constants.ArenaNames[0], _arenaDisplayViewModel.ArenaName);
+        }
+
+        [Fact]
+        public void TestArenaNamePropertyChanged()
+        {
+            const int expectedPropertyChangedInvocations = 1;
+            int propertyChangedInvocations = 0;
+
+            _arenaDisplayViewModel.PropertyChanged += (sender, e) => ++propertyChangedInvocations;
+
+            _arenaDisplayViewModel.ArenaName = "SomeArenaName";
+
+            Assert.Equal(expectedPropertyChangedInvocations, propertyChangedInvocations);
+        }
+
+        [Fact]
+        public void TestNextArenaIncrementsIndex()
+        {
+            const int expectedArenaIndex = 5;
+
+            CallNextArenaTimes(expectedArenaIndex);
+
+            Assert.Equal(Constants.ArenaNames[expectedArenaIndex], _arenaDisplayViewModel.ArenaName);
+        }
+
+        [Fact]
+        public void TestNextArenaWrapsIndex()
+        {
+            int arenaCount = Constants.ArenaNames.Length;
+            int requiredArenaIndex = arenaCount + 10;
+
+            CallNextArenaTimes(requiredArenaIndex);
+
+            Assert.Equal(Constants.ArenaNames[requiredArenaIndex % arenaCount], _arenaDisplayViewModel.ArenaName);
+        }
+
+        [Fact]
+        public void TestPreviousArenaDecrementsIndex()
+        {
+            const int expectedArenaIndex = 0;
+
+            _arenaDisplayViewModel.SelectNextArena();
+            _arenaDisplayViewModel.SelectPreviousArena();
+
+            Assert.Equal(Constants.ArenaNames[expectedArenaIndex], _arenaDisplayViewModel.ArenaName);
+        }
+
+        [Fact]
+        public void TestPreviousArenaWrapsIndex()
+        {
+            int expectedArenaIndex = Constants.ArenaNames.Length - 1;
+
+            _arenaDisplayViewModel.SelectPreviousArena();
+
+            Assert.Equal(Constants.ArenaNames[expectedArenaIndex], _arenaDisplayViewModel.ArenaName);
+        }
+
+        private void CallNextArenaTimes(int quantity)
+        {
+            for (int arenaIndex = 0; arenaIndex < quantity; ++arenaIndex)
+            {
+                _arenaDisplayViewModel.SelectNextArena();
+            }
+        }
+    }
+}
--- Instant Training UI/ViewModels/ArenaDisplayViewModel.cs
+++ Instant Training UI/ViewModels/ArenaDisplayViewModel.cs
@@ -4,5 +4,48 @@
 
     public class ArenaDisplayViewModel : ViewModelBase, IArenaDisplayViewModel
     {
+        private static readonly int ArenaNamesLength = Constants.ArenaNames.Length;
+
+        private int _arenaIndex;
+
+        public ArenaDisplayViewModel()
+        {
+            _arenaIndex = 0;
+
+            PickArenaNameFromIndex();
+        }
+
+        private string _arenaName;
+        public string ArenaName
+        {
+            get => _arenaName;
+
+            set
+            {
+                if (_arenaName == value) return;
+
+                _arenaName = value;
+                NotifyOfPropertyChange(nameof(ArenaName));
+            }
+        }
+
+        public void SelectNextArena()
+        {
+            _arenaIndex = ((_arenaIndex + 1) % ArenaNamesLength);
+
+            PickArenaNameFromIndex();
+        }
+
+        public void SelectPreviousArena()
+        {
+            _arenaIndex = (((_arenaIndex - 1) + ArenaNamesLength) % ArenaNamesLength);
+
+            PickArenaNameFromIndex();
+        }
+
+        private void PickArenaNameFromIndex()
+        {
+            ArenaName = Constants.ArenaNames[_arenaIndex];
+        }
     }
 }
