51f12b5f711587a6a29d114f4556d51ab1ef68f7
Aleksbgbg <kozichka01@abv.bg>
2019-04-12 15:34:12 +0100

    [MainViewModel] Add mod enabled/disabled checkbox.

--- Instant Training UI.Tests/ViewModels/MainViewModelTests.cs
+++ Instant Training UI.Tests/ViewModels/MainViewModelTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Instant.Training.UI.Tests.ViewModels
 {
+    using Instant.Training.UI.Services.Interfaces;
     using Instant.Training.UI.ViewModels;
     using Instant.Training.UI.ViewModels.Interfaces;
 
@@ -9,21 +10,41 @@
 
     public class MainViewModelTests
     {
+        private readonly Mock<IRconService> _rconServiceMock;
+
         private readonly Mock<IArenaDisplayViewModel> _arenaDisplayViewModelMock;
 
         private readonly MainViewModel _mainViewModel;
 
         public MainViewModelTests()
         {
+            _rconServiceMock = new Mock<IRconService>();
+
             _arenaDisplayViewModelMock = new Mock<IArenaDisplayViewModel>();
 
-            _mainViewModel = new MainViewModel(_arenaDisplayViewModelMock.Object);
+            _mainViewModel = new MainViewModel(_arenaDisplayViewModelMock.Object, _rconServiceMock.Object);
         }
 
         [Fact]
         public void TestArenaDisplayViewModelAssigned()
         {
             Assert.Equal(_arenaDisplayViewModelMock.Object, _mainViewModel.ArenaDisplayViewModel);
+        }
+
+        [Fact]
+        public void TestInstantTrainingEnabledByDefault()
+        {
+            Assert.True(_mainViewModel.EnableInstantTraining);
+        }
+
+        [Fact]
+        public void TestInstantTrainingToggleCallsRconService()
+        {
+            const bool modEnabled = false;
+
+            _mainViewModel.EnableInstantTraining = modEnabled;
+
+            _rconServiceMock.Verify(rconService => rconService.ToggleModEnabled(modEnabled));
         }
     }
 }
--- Instant Training UI/ViewModels/MainViewModel.cs
+++ Instant Training UI/ViewModels/MainViewModel.cs
@@ -1,14 +1,34 @@
 ﻿namespace Instant.Training.UI.ViewModels
 {
+    using Instant.Training.UI.Services.Interfaces;
     using Instant.Training.UI.ViewModels.Interfaces;
 
     public class MainViewModel : ViewModelBase, IMainViewModel
     {
-        public MainViewModel(IArenaDisplayViewModel arenaDisplayViewModel)
+        private readonly IRconService _rconService;
+
+        public MainViewModel(IArenaDisplayViewModel arenaDisplayViewModel, IRconService rconService)
         {
             ArenaDisplayViewModel = arenaDisplayViewModel;
+            _rconService = rconService;
         }
 
         public IArenaDisplayViewModel ArenaDisplayViewModel { get; }
+
+        private bool _enableInstantTraining = true;
+        public bool EnableInstantTraining
+        {
+            get => _enableInstantTraining;
+
+            set
+            {
+                if (_enableInstantTraining == value) return;
+
+                _enableInstantTraining = value;
+                NotifyOfPropertyChange(nameof(EnableInstantTraining));
+
+                _rconService.ToggleModEnabled(_enableInstantTraining);
+            }
+        }
     }
 }
