453165fb607275e1e31847277909b1200d4d23c4
Aleksbgbg <kozichka01@abv.bg>
2019-04-13 19:52:10 +0100

    [LibraryFoldersFile] Add class to parse the libraryfolders Steam file.

---
+++ Instant Training UI.Tests/Utilities/LibraryFolders/LibraryFoldersFileTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Instant.Training.UI.Tests.Utilities.LibraryFolders
+{
+    using Instant.Training.UI.Utilities.LibraryFolders;
+
+    using Xunit;
+
+    public class LibraryFoldersFileTests
+    {
+        [Fact]
+        public void TestParsesEmptyFile()
+        {
+            const string fileContents = @"""LibraryFolders""
+{
+}";
+
+            LibraryFoldersFile libraryFoldersFile = new LibraryFoldersFile(fileContents);
+
+            Assert.Empty(libraryFoldersFile.Entries);
+        }
+
+        [Fact]
+        public void TestParsesEntries()
+        {
+            const string fileContents = @"""LibraryFolders""
+{
+	""TimeNextStatsReport""		""1555173196""
+	""ContentStatsID""		""-7061122333939149038""
+	""1""		""Z:\\Steam""
+}";
+
+            LibraryFoldersFile libraryFoldersFile = new LibraryFoldersFile(fileContents);
+
+            Assert.Equal(3, libraryFoldersFile.Entries.Length);
+            Assert.Equal(@"Z:\Steam", libraryFoldersFile.Entries[2].Value);
+        }
+
+        [Fact]
+        public void TestAllEntriesValid()
+        {
+            const string fileContents = @"""LibraryFolders""
+{
+	""TimeNextStatsReport""		""1555173196""
+	""ContentStatsID""		""-7061122333939149038""
+	""1""		""Z:\\Steam""
+}";
+
+            LibraryFoldersFile libraryFoldersFile = new LibraryFoldersFile(fileContents);
+
+            Assert.All(libraryFoldersFile.Entries, entry => entry.IsValid());
+        }
+    }
+}
---
+++ Instant Training UI/Utilities/LibraryFolders/LibraryFoldersFile.cs
@@ -0,0 +1,19 @@
+﻿namespace Instant.Training.UI.Utilities.LibraryFolders
+{
+    using System.Linq;
+
+    public class LibraryFoldersFile
+    {
+        public LibraryFoldersFile(string fileContents)
+        {
+            Entries = fileContents.Split('\n')
+                                  .Select(line => line.TrimEnd('\r'))
+                                  .Where(line => line != string.Empty)
+                                  .Select(line => new LibraryFoldersEntry(line))
+                                  .Where(entry => entry.IsValid())
+                                  .ToArray();
+        }
+
+        public LibraryFoldersEntry[] Entries { get; }
+    }
+}
