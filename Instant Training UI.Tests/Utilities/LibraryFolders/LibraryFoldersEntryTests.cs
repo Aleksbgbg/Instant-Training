f23494861d877e7059b82344a7dd9b364682178c
Aleksbgbg <kozichka01@abv.bg>
2019-04-13 19:50:30 +0100

    [LibraryFoldersEntry] Add class to parse entries in the libraryfolders Steam file.

---
+++ Instant Training UI.Tests/Utilities/LibraryFolders/LibraryFoldersEntryTests.cs
@@ -0,0 +1,83 @@
+﻿namespace Instant.Training.UI.Tests.Utilities.LibraryFolders
+{
+    using Instant.Training.UI.Utilities.LibraryFolders;
+
+    using Xunit;
+
+    public class LibraryFoldersEntryTests
+    {
+        [Fact]
+        public void TestInvalidWhenWithOneComponent()
+        {
+            const string entry = "\"abcdef\"";
+
+            LibraryFoldersEntry libraryFoldersEntry = new LibraryFoldersEntry(entry);
+
+            Assert.False(libraryFoldersEntry.IsValid());
+        }
+
+        [Fact]
+        public void TestInvalidWhenComponentsWithoutQuotes()
+        {
+            const string entry = "abc def";
+
+            LibraryFoldersEntry libraryFoldersEntry = new LibraryFoldersEntry(entry);
+
+            Assert.False(libraryFoldersEntry.IsValid());
+        }
+
+        [Fact]
+        public void TestValidWithQuotesAroundComponents()
+        {
+            const string entry = "\"abc\" \"def\"";
+
+            LibraryFoldersEntry libraryFoldersEntry = new LibraryFoldersEntry(entry);
+
+            Assert.True(libraryFoldersEntry.IsValid());
+        }
+
+        [Fact]
+        public void TestValidWithWhiteSpace()
+        {
+            const string entry = "\t\t    \"abc\" \t\t\t\t\"def\"  ";
+
+            LibraryFoldersEntry libraryFoldersEntry = new LibraryFoldersEntry(entry);
+
+            Assert.True(libraryFoldersEntry.IsValid());
+        }
+
+        [Fact]
+        public void TestKeyValueNotSetWhenInvalid()
+        {
+            const string entry = "abc def";
+
+            LibraryFoldersEntry libraryFoldersEntry = new LibraryFoldersEntry(entry);
+
+            Assert.False(libraryFoldersEntry.IsValid());
+            Assert.Null(libraryFoldersEntry.Key);
+            Assert.Null(libraryFoldersEntry.Value);
+        }
+
+        [Fact]
+        public void TestParseKeyValue()
+        {
+            const string entry = "	\t\"ContentStatsID\"\t\t\"-7061122333939149038\"";
+
+            LibraryFoldersEntry libraryFoldersEntry = new LibraryFoldersEntry(entry);
+
+            Assert.Equal("ContentStatsID", libraryFoldersEntry.Key);
+            Assert.Equal("-7061122333939149038", libraryFoldersEntry.Value);
+        }
+
+        [Fact]
+        public void TestParsesPath()
+        {
+            const string entry = "\t\"1\"\t\t\"Z:\\\\Steam\"";
+
+            LibraryFoldersEntry libraryFoldersEntry = new LibraryFoldersEntry(entry);
+
+            Assert.Equal("1", libraryFoldersEntry.Key);
+            Assert.Equal(@"Z:\Steam", libraryFoldersEntry.Value);
+        }
+    }
+}
---
+++ Instant Training UI/Utilities/LibraryFolders/LibraryFoldersEntry.cs
@@ -0,0 +1,67 @@
+﻿namespace Instant.Training.UI.Utilities.LibraryFolders
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LibraryFoldersEntry
+    {
+        private readonly string[] _entryParts;
+
+        public LibraryFoldersEntry(string entry)
+        {
+            _entryParts = entry.Split(separator: (char[])null, StringSplitOptions.RemoveEmptyEntries);
+
+            if (IsValid())
+            {
+                Key = ParseComponent(0);
+                Value = ParseComponentOrPath(1);
+            }
+        }
+
+        public string Key { get; }
+
+        public string Value { get; }
+
+        public bool IsValid()
+        {
+            return _entryParts.Length == 2 && ComponentSurroundedByQuotes(0) && ComponentSurroundedByQuotes(1);
+        }
+
+        private bool ComponentSurroundedByQuotes(int index)
+        {
+            const string quote = "\"";
+
+            string component = _entryParts[index];
+
+            return component.StartsWith(quote) && component.EndsWith(quote);
+        }
+
+        private string ParseComponentOrPath(int index)
+        {
+            string component = ParseComponent(index);
+
+            if (IsPath(component))
+            {
+                component = Path.GetFullPath(component);
+            }
+
+            return component;
+        }
+
+        private bool IsPath(string component)
+        {
+            char[] invalidPathChars = Path.GetInvalidPathChars();
+            return Path.IsPathRooted(component) && !component.Any(invalidPathChars.Contains);
+        }
+
+        private string ParseComponent(int index)
+        {
+            const char quote = '"';
+
+            string component = _entryParts[index];
+
+            return component.Trim(quote);
+        }
+    }
+}
