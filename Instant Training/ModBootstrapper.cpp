7a1f6717710c378ceb0924f1be1b1d8dff81f134
Aleksbgbg <kozichka01@abv.bg>
2019-05-04 10:33:53 +0100

    [ModBootstrapper] Add class to bootstrap the mod by creating class instances.

---
+++ Instant Training/ModBootstrapper.cpp
@@ -0,0 +1,10 @@
+﻿#include "ModBootstrapper.h"
+
+ModBootstrapper::ModBootstrapper(const std::shared_ptr<GameWrapper>& gameWrapper)
+	:
+	commandWrapper{ gameWrapper },
+	eventWrapper{ gameWrapper },
+	gameEndedEventHook{ eventWrapper },
+	launchMapEventHandler{ commandWrapper, gameEndedEventHook }
+{
+}
---
+++ Instant Training/ModBootstrapper.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "Wrappers/CommandWrapperImpl.h"
+#include "Wrappers/EventWrapperImpl.h"
+#include "GameEndedEventHook.h"
+#include "LaunchMapEventHandler.h"
+
+class ModBootstrapper
+{
+public:
+	explicit ModBootstrapper(const std::shared_ptr<GameWrapper>& gameWrapper);
+
+private:
+	const CommandWrapperImpl commandWrapper;
+	const EventWrapperImpl eventWrapper;
+
+	const GameEndedEventHook gameEndedEventHook;
+	const LaunchMapEventHandler launchMapEventHandler;
+};
